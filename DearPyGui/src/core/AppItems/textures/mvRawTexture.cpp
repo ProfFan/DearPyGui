521fb5049887b25c47d65712a4f7b0d6b2cb8484
Fan Jiang <i@fanjiang.me>
2021-07-29 22:05:59 -0400

    update

--- DearPyGui/src/core/AppItems/textures/mvRawTexture.cpp
+++ DearPyGui/src/core/AppItems/textures/mvRawTexture.cpp
@@ -10,21 +10,22 @@ namespace Marvel {
 
 	void mvRawTexture::InsertParser(std::map<std::string, mvPythonParser>* parsers)
 	{
+		{
+			mvPythonParser parser(mvPyDataType::UUID, "Undocumented function", { "Textures", "Widgets" });
+			mvAppItem::AddCommonArgs(parser, (CommonParserArgs)(
+				MV_PARSER_ARG_ID)
+			);
 
-		mvPythonParser parser(mvPyDataType::UUID, "Undocumented function", { "Textures", "Widgets" });
-		mvAppItem::AddCommonArgs(parser, (CommonParserArgs)(
-			MV_PARSER_ARG_ID)
-		);
+			parser.addArg<mvPyDataType::Integer>("width");
+			parser.addArg<mvPyDataType::Integer>("height");
+			parser.addArg<mvPyDataType::FloatList>("default_value");
+			parser.addArg<mvPyDataType::Integer>("format", mvArgType::KEYWORD_ARG, "internal_dpg.mvFormat_Float_rgba", "Data format.");
+			parser.addArg<mvPyDataType::UUID>("parent", mvArgType::KEYWORD_ARG, "internal_dpg.mvReservedUUID_2", "Parent to add this item to. (runtime adding)");
+			parser.finalize();
 
-		parser.addArg<mvPyDataType::Integer>("width");
-		parser.addArg<mvPyDataType::Integer>("height");
-		parser.addArg<mvPyDataType::FloatList>("default_value");
-		parser.addArg<mvPyDataType::Integer>("format", mvArgType::KEYWORD_ARG, "internal_dpg.mvFormat_Float_rgba", "Data format.");
-		parser.addArg<mvPyDataType::UUID>("parent", mvArgType::KEYWORD_ARG, "internal_dpg.mvReservedUUID_2", "Parent to add this item to. (runtime adding)");
-		parser.finalize();
-
-		parsers->insert({ s_command, parser });
-
+			parsers->insert({ s_command, parser });
+		}
+		
 		{
 			mvPythonParser parser(mvPyDataType::UUID);
 			parser.addArg<mvPyDataType::UUID>("item");
